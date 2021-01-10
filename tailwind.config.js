c80007bbda0f6c42d22280d5ece745a7e0fe8043
samanito <hello.samanito@gmail.com>
2021-01-10 13:29:02 -0500

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'TitleFont': ['"Montserrat"', 'sans-serif'],
+        'BodyFont': ['"Merriweather"', 'serif'],
+      },
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem',
+        '9xl': '7rem',
+        '10xl': '8rem',
+      },
+      backgroundImage: theme => ({
+        'monster': "url('img/monster.jpg')",
+        })
+    },
+  },
+  variants: {},
+  plugins: [],
+}
