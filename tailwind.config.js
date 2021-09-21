a448207665f619cff5060e8d328c35b033b76057
samanito <hello.samanito@gmail.com>
2021-09-21 13:39:21 -0400

    Better color indigo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
@@ -14,6 +16,9 @@ module.exports = {
         '8xl': '6rem',
         '9xl': '7rem',
         '10xl': '8rem',
+      },
+      colors: {
+        orxe: colors.orange,
       },
       backgroundImage: theme => ({
         'monster': "url('img/monster.jpg')",
